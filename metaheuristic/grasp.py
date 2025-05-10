36b118800689af13a3122fd27ae1d4df840c0893
Gabriela Maccagnan <gabrielacmacc@gmail.com>
2025-05-10 14:16:42 -0300

    Print initial solution

--- metaheuristic/grasp.py
+++ metaheuristic/grasp.py
@@ -7,20 +7,25 @@ def grasp(instance, max_iterations, random_seed, alpha, max_time, start_time):
     random.seed(random_seed)
     best_solution = []
     best_value = 0
+    elapsed_time = 0
 
     for i in range(max_iterations):
         solution = random_greedy(instance, alpha)
         solution = local_search(solution)
         value = solution.get_value()
+        
+        if i == 0:
+            print(f"\nSolução inicial: {solution.solution}")
+            print(f"Valor inicial: {solution.value}\n")
+
+        if elapsed_time > max_time:
+            print(f"Tempo máximo atingido em {i} iterações")
+            break
 
         if value > best_value:
             best_solution = solution
             best_value = value
             elapsed_time = time.time() - start_time
             print(f"Nova solução encontrada: {solution.solution} com valor {best_value} em {elapsed_time:.2f} segundos")
-        
-        if elapsed_time > max_time:
-            print(f"Tempo máximo atingido em {i} iterações")
-            break
 
     return best_solution
