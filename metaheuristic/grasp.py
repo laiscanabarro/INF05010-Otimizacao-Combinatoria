57ed9b1885c598cfa95e838a6e73148682b49252
Gabriela Maccagnan <gabrielacmacc@gmail.com>
2025-05-10 10:10:06 -0300

    Use command line parameters

--- metaheuristic/grasp.py
+++ metaheuristic/grasp.py
@@ -1,11 +1,14 @@
+import time
+import random
 from local_search import local_search
 from random_greedy import random_greedy
 
-def grasp(instance, alpha, max_iterations):
+def grasp(instance, max_iterations, random_seed, alpha, max_time, start_time):
+    random.seed(random_seed)
     best_solution = []
     best_value = 0
 
-    for _ in range(max_iterations):
+    for i in range(max_iterations):
         solution = random_greedy(instance, alpha)
         solution = local_search(solution)
         value = solution.get_value()
@@ -13,5 +16,11 @@ def grasp(instance, alpha, max_iterations):
         if value > best_value:
             best_solution = solution
             best_value = value
+            elapsed_time = time.time() - start_time
+            print(f"Nova solução encontrada: {solution.solution} com valor {best_value} em {elapsed_time:.2f} segundos")
+        
+        if elapsed_time > max_time:
+            print(f"Tempo máximo atingido em {i} iterações")
+            break
 
     return best_solution
--- metaheuristic/main.py
+++ metaheuristic/main.py
@@ -1,37 +1,41 @@
 import os
 import time
+import argparse
 from reader import read_instance
 from grasp import grasp
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-instances_folder = os.path.join(current_dir, "..", "sinergias_instancias")
-
 def main():
-    total_time = 0
-
-    for i in range(1, 11):
-        instance_file = os.path.join(instances_folder, f"{i:02}.txt")
-        instance = read_instance(instance_file)
-
-        alpha = 0.15
-        max_iterations = 10
-
-        start_time = time.time()
-
-        solution = grasp(instance, alpha, max_iterations)
-
-        elapsed_time = time.time() - start_time
-        total_time += elapsed_time
-        
-        print(f"\nInstância {i:02}")
-        print(f"Número de items: {instance.n}")
-        print(f"Orçamento: {instance.budget}")
-        print(f"Solução otimizada: {solution.solution}")
-        print(f"Custo: {solution.total_cost}")
-        print(f"Valor: {solution.value}")
-        print(f"Tempo de execução: {elapsed_time:.2f} segundos")
     
-    print(f"\nTempo total das 10 instâncias: {total_time:.2f} segundos")
+    parser = argparse.ArgumentParser(description='GRASP - Problema de Sinergias')
+    parser.add_argument('-f', '--filepath', type=str, required=True, help='Caminho do arquivo de instâncias')
+    parser.add_argument('-m', '--iterations', type=int, required=True, help='Número máximo de iterações')
+    parser.add_argument('-s', '--seed', type=int, required=True, help='Semente de aleatoriedade')
+    parser.add_argument('-a', '--alpha', type=float, required=True, help='Parâmetro alpha de aleatoriedad')
+    parser.add_argument('-t', '--time', type=int, required=False, help='Tempo máximo de execução')
 
+    args = parser.parse_args()
+
+    file_path = args.filepath
+    max_iterations = args.iterations
+    random_seed = args.seed
+    alpha = args.alpha
+    max_time = args.time if args.time is not None else 300
+        
+    file_name = os.path.basename(file_path)
+    instance = read_instance(file_path)
+
+    print(f"\nArquivo: {file_name}")
+    print(f"Número de items: {instance.n}")
+    print(f"Orçamento: {instance.budget}")
+    
+    start_time = time.time()
+    solution = grasp(instance, max_iterations, random_seed, alpha, max_time, start_time)
+    elapsed_time = time.time() - start_time
+    
+    print(f"\nMelhor solução encontrada: {solution.solution}")
+    print(f"Custo: {solution.total_cost}")
+    print(f"Valor: {solution.value}")
+    print(f"Tempo total de execução: {elapsed_time:.2f} segundos\n")
+            
 if __name__ == "__main__":
     main()
